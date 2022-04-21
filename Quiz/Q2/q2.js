4d2bbe6961c1e2974e954fc77a4c04cee70b67fd
mehdi <90101330+mehdisheikhbaqeri@users.noreply.github.com>
2022-04-21 18:13:11 +0430

    Update q2.js

--- Quiz/Q2/q2.js
+++ Quiz/Q2/q2.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/Math/:num1/:num2", (req, res) => {
-  let { num1, num2 } = req.params;
+  let { num1, num2 } = req.query;
+  const result=num1*num2;
 
-  res.send(`<h1>Final is ${num1 * num2}</h1>`);
+  res.send(`<h1>Numbers is : ${num1} and ${num2}, Result is = ${result}</h1>`);
 });
 
 app.listen("3066", (req, res) => {
